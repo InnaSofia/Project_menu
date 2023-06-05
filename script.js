54b0659987b7a1b7ed5012a8590b0e59f7f47e3d
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-06-05 11:50:14 +0300

    js

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 let isOpen = false
 function openMenu(){
     const  conteinerMenu = document.getElementById("myConteinerMenu")
+    let changeImage = document.getElementById("image")
 
 
 
     if(isOpen){
         conteinerMenu.style.display = "none"
         isOpen = false
+        changeImage.src = 'menu.png'
     }else{
         conteinerMenu.style.display = "flex"
         isOpen = true
+        changeImage.src = 'close.png'
     }
 
-    document.getElementById("buttonMenu").style.backgroundColor = "#ffffff"
-    document.getElementById("buttonMenu").style.borderRadius = "100%"
-    document.getElementById("buttonMenu").style.width = "50px"
-    document.getElementById("buttonMenu").style.height = "50px"
-    document.getElementById("buttonMenu").style.border = "1px solid #ff6b35"
-    document.getElementById("buttonMenu").style.position = "absolute"
-    document.getElementById("buttonMenu").style.top = "2%"
-    document.getElementById("buttonMenu").style.right = "3%"
-    document.getElementById("buttonMenu").style.active.background-color ; " #fb7447"
+    
 }
 
 
